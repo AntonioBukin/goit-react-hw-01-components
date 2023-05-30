55fdcdf2725d44ff4b46605a881e2b6c02588ae9
AntonioBukin <bav1905@gmail.com>
2023-05-30 11:12:35 +0300

    add info

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,3 +18,33 @@
 import Profile from './Profile';
 import user from '../data/user.json';
 import Statistics from './Statistics';
+import data from '../data/data.json';
+import FriendList from './FriendList';
+import friends from '../data/friends.json';
+import TransactionTable from './TransactionTable';
+import transactions from '../data/transactions.json';
+
+export default function App() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 23,
+      }}
+    >
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionTable items={transactions} />
+    </div>
+  );
+}
